cbea7e1eeb6dd8b3ff04db673b02d273e8846acb
Aman Kumar <116003112+AmanKumar2911@users.noreply.github.com>
2023-10-16 16:46:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function glow() {
+
+    const div = document.querySelector("div");
+    const image2 = document.createElement('img');
+    image2.setAttribute('src', "bulb2.jpg");
+    div.innerHTML = "";
+    div.appendChild(image2);
+
+}
